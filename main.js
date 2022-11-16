97c67f89dd2c862fcc873651be4cc84b157a6b09
clodo <104923248+clodoN1109@users.noreply.github.com>
2022-11-16 11:52:22 -0300

    Various updates and fixes, including change of API for one under https protocol.

--- main.js
+++ main.js
@@ -1,33 +1,53 @@
-
 const iss_data = [0, 0, 0];
 
-const address = fetch("http://api.open-notify.org/iss-now.json")
-.then((response) => response.json())
-.then((iss) => {
+function timestampToDateConversion(timestamp){
 
-  console.log(iss);
+  let epoch = Math.floor(new Date().getTime(timestamp)/1000.0); 
+  var myDate = new Date( epoch *1000);
+  myDate.toGMTString()+"<br>"+myDate.toLocaleString();  
 
-  iss_data[0] = iss.iss_position.latitude;
-  iss_data[1] = iss.iss_position.longitude;
-  iss_data[2] = iss.timestamp;
+  return myDate;
 
-  console.log("iss_data: ", iss_data);
+}
 
-  document.getElementById("latitude").value = iss_data[0];
-  document.getElementById("longitude").value = iss_data[1];
-  document.getElementById("time").value = iss_data[2];
+function fetchIssData(){
 
-  let latitude = Number(iss_data[0]) - 90;
-  let longitude = Number(iss_data[1]) + 180;
-  let time = iss_data[2];
+  fetch("https://api.wheretheiss.at/v1/satellites/25544")
+  .then((response) => response.json())
+  .then((iss) => {
 
-  let actual_position = document.getElementsByClassName("satellite")[0].style.transform;
-  console.log("actual position: ",actual_position);
+    console.log(iss);
 
-  document.getElementsByClassName("satellite")[0]
-  .style.transform = "translate(" + longitude + "px, " + latitude +  "px)";
+    iss_data[0] = iss.latitude;
+    iss_data[1] = iss.longitude;
+    iss_data[2] = timestampToDateConversion(Number(iss.timestamp));
 
 
+    console.log("iss_data: ", iss_data);
 
-});
+    document.getElementById("latitude").value = iss_data[0];
+    document.getElementById("longitude").value = iss_data[1];
+    document.getElementById("time").value = iss_data[2];
+
+    let latitude = (Number(iss_data[0]) - 90)*(-2.2222);
+    let longitude = (Number(iss_data[1]) + 180)*(2.2222);
+    let time = iss_data[2];
+
+    let actual_position = document.getElementsByClassName("satellite")[0].style.transform;
+
+
+    document.getElementsByClassName("satellite")[0]
+    .style.transform = "translate(" + longitude + "px, " + latitude +  "px)";
+
+  setTimeout(fetchIssData,4000);
+
+
+
+  });
+
+}
+
+
+fetchIssData();
+
 
