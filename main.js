513a2f1b110464b61f73b64111ba2ed75e6d28e5
clodo <104923248+clodoN1109@users.noreply.github.com>
2022-11-16 23:43:56 -0300

    Several design refinements and improvements.

--- main.js
+++ main.js
@@ -10,6 +10,21 @@ function timestampToDateConversion(timestamp){
 
 }
 
+function updateWikiInfo(){
+
+    let info1 = "The International Space Station (ISS) is the largest modular space station currently in low Earth orbit.";
+    let info2 = "The first ISS component was launched in 1998, and the first long-term residents arrived on 2 November 2000 after being launched from the Baikonur Cosmodrome on 31 October 2000."
+    let info3 ="The ISS programme evolved from the Space Station Freedom, a 1984 American proposal to construct a permanently crewed Earth-orbiting station,[15] and the contemporaneous Soviet/Russian Mir-2 proposal from 1976 with similar aims.";
+    let info4 ="The station is divided into two sections: the Russian Orbital Segment (ROS) is operated by Russia, while the United States Orbital Segment (USOS) is run by the United States as well as by the other states.";
+    let info5 = " As of April 2022, 251 astronauts, cosmonauts, and space tourists from 20 different nations have visited the space station, many of them multiple times.";
+    let infoList = [info1, info2, info3, info4, info5];
+
+    document.getElementsByClassName("wikiInfo")[0].textContent = infoList[Math.floor(Math.random() * infoList.length)];
+
+
+    setTimeout(updateWikiInfo,7000);
+}
+
 function copyCoordinates(){
 
   let copyInfo = document.getElementById("latitude").value + ", " +
@@ -30,7 +45,7 @@ function fetchIssData(){
   .then((response) => response.json())
   .then((iss) => {
 
-    console.log(iss);
+/*    console.log(iss);*/
 
     iss_data[0] = iss.latitude;
     iss_data[1] = iss.longitude;
@@ -38,7 +53,7 @@ function fetchIssData(){
     iss_data[3] = iss.altitude;
 
 
-    console.log("iss_data: ", iss_data);
+    /*console.log("iss_data: ", iss_data);*/
 
     document.getElementById("latitude").value = iss_data[0];
     document.getElementById("longitude").value = iss_data[1];
@@ -64,6 +79,7 @@ function fetchIssData(){
 }
 
 
+setTimeout(updateWikiInfo,7000);
 fetchIssData();
 
 
